58ff0c82621905624e5997454c8ecfcc323902b1
Augusto Roman <aroman@gmail.com>
2016-08-30 11:36:35 -0700

    Changed how the adapter subpackages work.
    The helper functions don't make as much sense as just wrapping the
    Middlware object, and the wrapping is pretty thin.

--- httprouter_sandwich/httprouter.go
+++ httprouter_sandwich/httprouter.go
@@ -10,15 +10,37 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func Adapt(mw sandwich.Middleware) sandwich.Middleware {
-	return sandwich.Middleware(chain.Chain(mw).Reserve((httprouter.Params)(nil)))
+func New() Middleware {
+	return Middleware(
+		chain.Chain(sandwich.New()).Reserve(
+			(httprouter.Params)(nil)))
+}
+func TheUsual() Middleware {
+	return Middleware(
+		chain.Chain(sandwich.TheUsual()).Reserve(
+			(httprouter.Params)(nil)))
 }
 
-func Handler(mw sandwich.Middleware) func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		err := chain.Chain(mw).Run((*http.ResponseWriter)(&w), r, p)
-		if err != nil {
-			panic(err)
-		}
+type Middleware sandwich.Middleware
+
+func (m Middleware) w() sandwich.Middleware { return sandwich.Middleware(m) }
+
+func (m Middleware) Provide(val interface{}) Middleware { return Middleware(m.w().Provide(val)) }
+func (m Middleware) ProvideAs(val, ifacePtr interface{}) Middleware {
+	return Middleware(m.w().ProvideAs(val, ifacePtr))
+}
+
+func (m Middleware) With(handlers ...interface{}) Middleware {
+	return Middleware(m.w().With(handlers...))
+}
+func (m Middleware) OnErr(handler interface{}) Middleware { return Middleware(m.w().OnErr(handler)) }
+func (m Middleware) Wrap(before, after interface{}) Middleware {
+	return Middleware(m.w().Wrap(before, after))
+}
+func (m Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	err := chain.Chain(m).Run((*http.ResponseWriter)(&w), r, p)
+	if err != nil {
+		panic(err)
 	}
 }
+func (m Middleware) Code(pkg, funcName string) string { return m.w().Code(pkg, funcName) }
--- martini_sandwich/martini.go
+++ martini_sandwich/martini.go
@@ -10,18 +10,40 @@ import (
 	"github.com/go-martini/martini"
 )
 
-func Adapt(mw sandwich.Middleware) sandwich.Middleware {
-	c := chain.Chain(mw).
+func New() Middleware {
+	c := chain.Chain(sandwich.New()).
 		Reserve((martini.Params)(nil)).
 		Reserve((*martini.ResponseWriter)(nil))
-	return sandwich.Middleware(c)
+	return Middleware(c)
+}
+func TheUsual() Middleware {
+	c := chain.Chain(sandwich.TheUsual()).
+		Reserve((martini.Params)(nil)).
+		Reserve((*martini.ResponseWriter)(nil))
+	return Middleware(c)
 }
 
-func Handler(mw sandwich.Middleware) func(w martini.ResponseWriter, r *http.Request, p martini.Params) {
-	return func(w martini.ResponseWriter, r *http.Request, p martini.Params) {
-		err := chain.Chain(mw).Run((*martini.ResponseWriter)(&w), (*http.ResponseWriter)(&w), r, p)
-		if err != nil {
-			panic(err)
-		}
+type Middleware sandwich.Middleware
+
+func (m Middleware) w() sandwich.Middleware { return sandwich.Middleware(m) }
+
+func (m Middleware) Provide(val interface{}) Middleware { return Middleware(m.w().Provide(val)) }
+func (m Middleware) ProvideAs(val, ifacePtr interface{}) Middleware {
+	return Middleware(m.w().ProvideAs(val, ifacePtr))
+}
+
+func (m Middleware) With(handlers ...interface{}) Middleware {
+	return Middleware(m.w().With(handlers...))
+}
+func (m Middleware) OnErr(handler interface{}) Middleware { return Middleware(m.w().OnErr(handler)) }
+func (m Middleware) Wrap(before, after interface{}) Middleware {
+	return Middleware(m.w().Wrap(before, after))
+}
+func (m Middleware) ServeHTTP(w martini.ResponseWriter, r *http.Request, p martini.Params) {
+	rw := http.ResponseWriter(w)
+	err := chain.Chain(m).Run((*martini.ResponseWriter)(&w), (*http.ResponseWriter)(&rw), r, p)
+	if err != nil {
+		panic(err)
 	}
 }
+func (m Middleware) Code(pkg, funcName string) string { return m.w().Code(pkg, funcName) }
