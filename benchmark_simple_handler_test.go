70186241b6f7d2c5eaf412d38f274193ac98ad48
Augusto Roman <aroman@gmail.com>
2018-02-07 21:55:03 -0700

    Add benchmarks measuring overhead of sandwich over raw handlers.
    Benchmarks compare the following:
    - direct calling of a simple handler (RawHTTP)
    - calling of the simple handler using a bare sandwich.Middleware
      stack created using sandwich.New() (Dynamic_Bare)
    - calling of the simple handler using TheUsual sandwich.Middleware
      stack (Dynamic_TheUsual)
    - calling the bare sandwich.Middleware stack via the generated code
      (Generated_Bare)
    - calling TheUsual stack via the generated code (Generated_TheUsual)
    
    Each of these scenarios is tested with three very simple handlers:
     - A trivial handler that just writes "Hello there!" to the response.
     - A trivial handler that just sets the status code to 204 (no content).
     - A trivial handler that encodes some simple JSON to the output.
    
    The benchmark summary on my system (raw results below) are:
     * The generated code for the bare stack imposes no measurable
       overhead over direct calls, as expected.
     * The generated code for TheUsual stack costs ~3µs.
     * The dynamic code appears to cost 2-3µs per call. So for the bare
       stack, that's only 1 call and so costs about 3µs, but for TheUsual
       stack it adds 4 more calls so the total is ~10µs.
    
    For a complicated program stack involving up to 20 calls, that's up to
    ~60µs overhead per call. So if a server typically responds in 1 ms
    (including all network traffic), that's about a 6% performance impact.
    
    Raw benchmark numbers:
      Benchmark_Hello_RawHTTP-4                    1000000        1840 ns/op
      Benchmark_Hello_Generated_Bare-4             1000000        1630 ns/op
      Benchmark_Hello_Generated_TheUsual-4          300000        4405 ns/op
      Benchmark_Hello_Dynamic_Bare-4                500000        3829 ns/op
      Benchmark_Hello_Dynamic_TheUsual-4            200000       12628 ns/op
    
      Benchmark_Write204_RawHTTP-4                 5000000         410 ns/op
      Benchmark_Write204_Generated_Bare-4          5000000         357 ns/op
      Benchmark_Write204_Generated_TheUsual-4       500000        2824 ns/op
      Benchmark_Write204_Dynamic_Bare-4            1000000        2294 ns/op
      Benchmark_Write204_Dynamic_TheUsual-4         200000        9002 ns/op
    
      Benchmark_SendJson_RawHTTP-4                  500000        2835 ns/op
      Benchmark_SendJson_Generated_Bare-4           500000        2839 ns/op
      Benchmark_SendJson_Generated_TheUsual-4       300000        5921 ns/op
      Benchmark_SendJson_Dynamic_Bare-4             300000        5379 ns/op
      Benchmark_SendJson_Dynamic_TheUsual-4         100000       13591 ns/op

---
+++ benchmark_simple_handler_test.go
@@ -0,0 +1,190 @@
+package sandwich
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+// Sample data to JSON-encode for benchmarking.
+var userInfo = struct {
+	Id        uint64 `json:"id"`
+	Name      string `json:"name"`
+	Email     string `json:"email"`
+	AvatarUrl string `json:"avatar_url"`
+}{
+	12345467, "John Doe", "john.doe@example.com", "https://www.example.com/users/john.doe/image",
+}
+
+func write204(w http.ResponseWriter, r *http.Request) { w.WriteHeader(http.StatusNoContent) }
+func hello(w http.ResponseWriter, r *http.Request)    { w.Write([]byte("Hello there!")) }
+func sendjson(w http.ResponseWriter, r *http.Request) { json.NewEncoder(w).Encode(userInfo) }
+
+func makeWrite204_TheUsual() Middleware { return TheUsual().With(NoLog, write204) }
+func makeWrite204_Bare() Middleware     { return New().With(write204) }
+func makeHello_TheUsual() Middleware    { return TheUsual().With(NoLog, hello) }
+func makeHello_Bare() Middleware        { return New().With(hello) }
+func makeSendJson_TheUsual() Middleware { return TheUsual().With(NoLog, sendjson) }
+func makeSendJson_Bare() Middleware     { return New().With(sendjson) }
+
+func bench(N int, h http.Handler) {
+	req := httptest.NewRequest("GET", "/", nil)
+	for i := 0; i < N; i++ {
+		w := httptest.NewRecorder()
+		h.ServeHTTP(w, req)
+	}
+}
+
+// Just to shorten the following benchmark functions:
+type Handler = http.HandlerFunc
+
+func Benchmark_Hello_RawHTTP(b *testing.B)            { bench(b.N, Handler(hello)) }
+func Benchmark_Hello_Generated_Bare(b *testing.B)     { bench(b.N, Handler(gen_hello_bare())) }
+func Benchmark_Hello_Generated_TheUsual(b *testing.B) { bench(b.N, Handler(gen_hello_theusual())) }
+func Benchmark_Hello_Dynamic_Bare(b *testing.B)       { bench(b.N, makeHello_Bare()) }
+func Benchmark_Hello_Dynamic_TheUsual(b *testing.B)   { bench(b.N, makeHello_TheUsual()) }
+
+func Benchmark_Write204_RawHTTP(b *testing.B)            { bench(b.N, Handler(write204)) }
+func Benchmark_Write204_Generated_Bare(b *testing.B)     { bench(b.N, Handler(gen_write204_bare())) }
+func Benchmark_Write204_Generated_TheUsual(b *testing.B) { bench(b.N, Handler(gen_write204_theusual())) }
+func Benchmark_Write204_Dynamic_Bare(b *testing.B)       { bench(b.N, makeWrite204_Bare()) }
+func Benchmark_Write204_Dynamic_TheUsual(b *testing.B)   { bench(b.N, makeWrite204_TheUsual()) }
+
+func Benchmark_SendJson_RawHTTP(b *testing.B)            { bench(b.N, Handler(sendjson)) }
+func Benchmark_SendJson_Generated_Bare(b *testing.B)     { bench(b.N, Handler(gen_sendjson_bare())) }
+func Benchmark_SendJson_Generated_TheUsual(b *testing.B) { bench(b.N, Handler(gen_sendjson_theusual())) }
+func Benchmark_SendJson_Dynamic_Bare(b *testing.B)       { bench(b.N, makeSendJson_Bare()) }
+func Benchmark_SendJson_Dynamic_TheUsual(b *testing.B)   { bench(b.N, makeSendJson_TheUsual()) }
+
+func TestGenBenchmarkCode(t *testing.T) {
+	gen_functions := []string{
+		makeHello_Bare().Code("sandwich", "gen_hello_bare"),
+		makeHello_TheUsual().Code("sandwich", "gen_hello_theusual"),
+		makeWrite204_Bare().Code("sandwich", "gen_write204_bare"),
+		makeWrite204_TheUsual().Code("sandwich", "gen_write204_theusual"),
+		makeSendJson_Bare().Code("sandwich", "gen_sendjson_bare"),
+		makeSendJson_TheUsual().Code("sandwich", "gen_sendjson_theusual"),
+	}
+	gen_code := "\n\n" + strings.Join(gen_functions, "\n\n") + "\n\n"
+	t.Log("Generated code: " + gen_code)
+}
+
+// ---------------------------------------------------------------------------
+// Auto-generated code below here
+// ---------------------------------------------------------------------------
+
+func gen_hello_bare() func(
+	rw http.ResponseWriter,
+	req *http.Request,
+) {
+	return func(
+		rw http.ResponseWriter,
+		req *http.Request,
+	) {
+		hello(rw, req)
+
+	}
+}
+
+func gen_hello_theusual() func(
+	rw http.ResponseWriter,
+	req *http.Request,
+) {
+	return func(
+		rw http.ResponseWriter,
+		req *http.Request,
+	) {
+		var pResponseWriter *ResponseWriter
+		rw, pResponseWriter = WrapResponseWriter(rw)
+
+		var pLogEntry *LogEntry
+		pLogEntry = StartLog(req)
+
+		defer func() {
+			(*LogEntry).Commit(pLogEntry, pResponseWriter)
+		}()
+
+		NoLog(pLogEntry)
+
+		hello(rw, req)
+
+	}
+}
+
+func gen_write204_bare() func(
+	rw http.ResponseWriter,
+	req *http.Request,
+) {
+	return func(
+		rw http.ResponseWriter,
+		req *http.Request,
+	) {
+		write204(rw, req)
+
+	}
+}
+
+func gen_write204_theusual() func(
+	rw http.ResponseWriter,
+	req *http.Request,
+) {
+	return func(
+		rw http.ResponseWriter,
+		req *http.Request,
+	) {
+		var pResponseWriter *ResponseWriter
+		rw, pResponseWriter = WrapResponseWriter(rw)
+
+		var pLogEntry *LogEntry
+		pLogEntry = StartLog(req)
+
+		defer func() {
+			(*LogEntry).Commit(pLogEntry, pResponseWriter)
+		}()
+
+		NoLog(pLogEntry)
+
+		write204(rw, req)
+
+	}
+}
+
+func gen_sendjson_bare() func(
+	rw http.ResponseWriter,
+	req *http.Request,
+) {
+	return func(
+		rw http.ResponseWriter,
+		req *http.Request,
+	) {
+		sendjson(rw, req)
+
+	}
+}
+
+func gen_sendjson_theusual() func(
+	rw http.ResponseWriter,
+	req *http.Request,
+) {
+	return func(
+		rw http.ResponseWriter,
+		req *http.Request,
+	) {
+		var pResponseWriter *ResponseWriter
+		rw, pResponseWriter = WrapResponseWriter(rw)
+
+		var pLogEntry *LogEntry
+		pLogEntry = StartLog(req)
+
+		defer func() {
+			(*LogEntry).Commit(pLogEntry, pResponseWriter)
+		}()
+
+		NoLog(pLogEntry)
+
+		sendjson(rw, req)
+
+	}
+}
